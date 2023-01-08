a095be897810a034de8ec7c2f17c73ee39749bf2
waihura <waihura.m@gmail.com>
2023-01-08 18:51:32 +0300

    Media message by ID

--- src/Message.php
+++ src/Message.php
@@ -12,7 +12,7 @@ class Message extends service
     /**
      * It sends a text message to a recipient
      * 
-     * @param content an array containing the following keys:['to'] & ['message']
+     * @param content This is the content of the message.
      * 
      * @return The response from the API.
      */
@@ -41,7 +41,7 @@ class Message extends service
     /**
      * It sends a text message to a recipient
      * 
-     * @param content an array containing the following keys:['to'], ['message_id'] & ['message']
+     * @param content This is the content of the message.
      * 
      * @return The response from the API.
      */
@@ -98,6 +98,38 @@ class Message extends service
             'type' => $content['type'],
             $content['type'] => [
                 'link' => $content['url']
+            ]
+        ];
+
+        $response = $this->client->post($data);
+
+        return $this->success($response);
+    }
+
+    /**
+     * Send a media message to a recipient by media ID
+     * 
+     * @param content This is the content of the message.
+     * 
+     * @return The response from the API.
+     */
+    public function sendMediaMessageByID($content)
+    {
+        if (empty($content['to']) || empty($content['type']) || empty($content['media_id'])) {
+            return $this->error('recipient, media type and media ID must be defined');
+        }
+
+        if (!in_array($content['type'], ['image', 'document', 'audio', 'sticker', 'video'])) {
+            return $this->error('media type is not supported');
+        }
+
+        $data = [
+            'messaging_product' => 'whatsapp',
+            'recipient_type' => 'individual',
+            'to' => $content['to'],
+            'type' => $content['type'],
+            $content['type'] => [
+                'id' => $content['media_id']
             ]
         ];
 
