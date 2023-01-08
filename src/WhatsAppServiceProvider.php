015b39474d946e4d9c6861c26f00b2d2ddaa1e2a
waihura <waihura.m@gmail.com>
2023-01-08 19:46:06 +0300

    Update service provider

--- src/WhatsAppServiceProvider.php
+++ src/WhatsAppServiceProvider.php
@@ -6,5 +6,21 @@ use Illuminate\Support\ServiceProvider;
 
 class WhatsAppServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        // Register class in the service container
+        $this->app->bind('whatsapp', function ($app) {
+            return new WhatsApp();
+        });
+    }
 
+    /**
+     * It tells Laravel to publish the config file to the config folder
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/whatsapp.php' => config_path('whatsapp.php'),
+        ], 'config');
+    }
 }
